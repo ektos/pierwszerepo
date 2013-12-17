a335ad2d432a6cc9941e8085f7c9f4321d69008e
inf109793 <inf109793@LAB-DBM-10.cs.put.poznan.pl>
2013-12-17 15:15:50 +0300

    Pierwsza rewizja

---
+++ src/testgit/Testgit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author inf109793
+ */
+public class Testgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
